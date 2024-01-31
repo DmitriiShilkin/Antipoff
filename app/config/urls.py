390f29dd0f8a6260169e1cb24a5d20a6f7158607
Dmitrii Shilkin <dmitriishilkin@gmail.com>
2024-01-31 18:34:51 +0300

    Add imports in urls Add container_names to docker-compose Rename char_set to charset in nginx

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers, permissions
 
 from drf_yasg.views import get_schema_view
